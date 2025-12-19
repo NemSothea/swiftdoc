a85a0205ade4af0e59461e760632718ce4e1e533
nemsothea <nemsothea13@gmail.com>
2025-12-19 16:10:08 +0700

    add lession week07

---
+++ Week07/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by sothea007 on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Week07/SwiftUIExample/SwiftUIExampleTests/SwiftUIExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleTests.swift
+//  SwiftUIExampleTests
+//
+//  Created by sothea007 on 19/12/25.
+//
+
+import Testing
+@testable import SwiftUIExample
+
+struct SwiftUIExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
